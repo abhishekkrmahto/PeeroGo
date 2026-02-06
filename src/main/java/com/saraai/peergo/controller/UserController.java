a262632b3c286016177d4c4cd5801a78d1a36578
avishekkrmahto <abhishek.mahto2025@gmail.com>
2026-02-06 21:26:51 +0530

    learned HTTP methods GET POST PUSH and POST and how to use postman for api hitting

--- src/main/java/com/saraai/peergo/controller/UserController.java
+++ src/main/java/com/saraai/peergo/controller/UserController.java
@@ -1,11 +1,11 @@
 package com.saraai.peergo.controller;
 
 import com.saraai.peergo.models.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -14,12 +14,14 @@ public class UserController {
         List<User>users = new ArrayList<>();
 
         User user1 = new User();
+        user1.setId(1);
         user1.setFirstName("Abhisek");
         user1.setLastName("Mahto");
         user1.setEmail("avishek@gmail.com");
         user1.setPassword("Abhishek@2025");
 
         User user2 = new User();
+        user2.setId(2);
         user2.setFirstName("Abhisek");
         user2.setLastName("Mahto");
         user2.setEmail("avishek@gmail.com");
@@ -29,5 +31,63 @@ public class UserController {
         users.add(user2);
 
         return users;
+    }
+
+
+
+    @GetMapping("/users/{userId}")
+    public User getUsersById(@PathVariable("userId")Integer id){
+        List<User>users = new ArrayList<>();
+
+        User user1 = new User();
+        user1.setId(id);
+        user1.setFirstName("Abhisek");
+        user1.setLastName("Mahto");
+        user1.setEmail("avishek@gmail.com");
+        user1.setPassword("Abhishek@2025");
+
+        return user1;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user){
+        User newUser = new User();
+        newUser.setId(user.getId());
+        newUser.setPassword((user.getPassword()));
+        newUser.setEmail(user.getEmail());
+        newUser.setFirstName(user.getFirstName());
+        newUser.setLastName(user.getLastName());
+        return  newUser;
+    }
+
+
+
+    @PutMapping("/users")
+    public User updateUser(@RequestBody User user) {
+        User newUser = new User();
+        newUser.setId(12);
+        newUser.setFirstName("Abhishek");
+        newUser.setLastName("Mahto");
+        newUser.setEmail("avishek12@gmail.com");
+        newUser.setPassword("My password");
+        if (user.getFirstName() != null) {
+            newUser.setFirstName(user.getFirstName());
+        }
+        if (user.getLastName() != null) {
+            newUser.setLastName(user.getLastName());
+        }
+        if (user.getEmail() != null) {
+            newUser.setEmail(user.getEmail());
+        }
+        if (user.getPassword() != null) {
+            newUser.setPassword(user.getPassword());
+        }
+        return newUser;
+    }
+
+
+    @DeleteMapping("/users/{id}")
+    public String deleteUser(@PathVariable("id") Integer id){
+        return "user id "+id+" deleted successfully";
     }
 }
--- src/main/java/com/saraai/peergo/models/User.java
+++ src/main/java/com/saraai/peergo/models/User.java
@@ -2,7 +2,7 @@ package com.saraai.peergo.models;
 
 public class User {
 
-
+    private Integer id;
     private String firstName;
     private String lastName;
     private String email;
@@ -11,13 +11,21 @@ public class User {
     public User() {
     }
 
-    public User(String firstName, String lastName, String email, String password) {
+    public User(Integer id, String firstName, String lastName, String email, String password) {
+        this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.password = password;
     }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public String getFirstName() {
         return firstName;
